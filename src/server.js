951600b84ee20f45b2d211cf39ebab73b96cf732
jandsonanjos <jandsonanjos@hotmail.com>
2026-03-16 00:33:14 -0300

    adicionando api inicial na web com o WS

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {getContainers, getContainerStats, formatStats} = require('./docker')
-
+const http = require('http');
+const WebSocket = require('ws');
 const app = express();
 const PORT = 3000;
 
@@ -24,6 +25,40 @@ app.get('/api/containers', async (req, res) => {
     }
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})  
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
+
+wss.on('connection', (ws) => {
+    console.log('Client connected via WebSocket')
+
+    const sendStats = async () => {
+        try {
+            const containers = await getContainers()
+            const statsPromises = containers.map(async (container) => {
+                const rawStats = await getContainerStats(container.Id)
+                return {
+                    id: container.Id,
+                    name: container.Names[0].replace(/^\//, ''), // Remove leading slash
+                    stats: formatStats(rawStats)
+                }
+            })
+
+            const containersWithStats = await Promise.all(statsPromises)
+            ws.send(JSON.stringify(containersWithStats))
+        } catch (error) {
+            console.error('Error fetching container data:', error)
+        }
+          const interval = setInterval(sendStats, 2000)
+          sendStats() // envia imediatamente na conexão, sem esperar 2s
+            ws.on('close', () => {
+            clearInterval(interval)
+            console.log('Client disconnected')
+    })
+    }
+
+    
+})
+    server.listen(PORT, () => {
+        console.log(`Monitor rodando em http://localhost:${PORT}`)
+
+})
